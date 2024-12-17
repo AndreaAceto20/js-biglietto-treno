73a7ff1bdc5a2ae7425d268b095d7095fb6cfde4
Andrea Aceto <andrea.aceto20@gmail.com>
2024-12-17 13:58:51 +0100

    JS Logic

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// Input dati utenti
+// Calcolo prezzo biglietto in base ai km (2 cifre decimali)
+// SE il passeggero ha meno di 12 anni, applica uno sconto del 20%
+// ALTRIMENTI SE il passeggero ha più di 65 anni, applica uno sconto del 40%
