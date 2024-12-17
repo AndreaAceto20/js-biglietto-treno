c276db552c897f74bea59f5958139794ef9eedaf
Andrea Aceto <andrea.aceto20@gmail.com>
2024-12-17 14:27:04 +0100

    Limited decimals at 2

--- js/script.js
+++ js/script.js
@@ -12,14 +12,14 @@ let prezzoBiglietto = (km * prezzoKm).toFixed(2);
 // SE il passeggero ha meno di 12 anni, applica uno sconto del 20%
 if (age <= 12){
     sconto = 2/10;
-    prezzoBiglietto = (prezzoBiglietto - (prezzoBiglietto * sconto));
+    prezzoBiglietto = (prezzoBiglietto - (prezzoBiglietto * sconto)).toFixed(2);
     console.log(prezzoBiglietto);
 }
 
 // ALTRIMENTI SE il passeggero ha più di 65 anni, applica uno sconto del 40%
 else if(age >= 65){
     sconto = 4/10;
-    prezzoBiglietto = (prezzoBiglietto - (prezzoBiglietto * sconto));
+    prezzoBiglietto = (prezzoBiglietto - (prezzoBiglietto * sconto)).toFixed(2);
     console.log(prezzoBiglietto);
 }
 // ALTRIMENTI riporta il prezzo base
